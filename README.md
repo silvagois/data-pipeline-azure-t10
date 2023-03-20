a608518eeea5ead07fd8cb6596d5c420a35c10c3
silvagois <marcossilvagois@gmail.com>
2023-03-19 22:15:08 -0300

    Finalização projeto T10

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# data-pipeline-azure-t10
-Criação de Data Pipeline Notas fiscais usando Azure como provedora
+
+## Projeto data-pipeline-azure-t10
+
+- Criação de um pipeline na Azure onde extraia dados de nota fiscal xml de um container/bucket e faça transformações e implementações nas zonas corretas do Data Lake
+
+## ARQUITETURA:
+    - A infra básica foi feita via Terraform, onde foi criado o resource group, storage account, e os containers landing, raw, processing e curated,
+
+    - Landing: Seria uma zona transitoria, onde o Data Factory por exemplo fazia a ingestão de um arquivo xml nessa camada, seria feito algumas validações de catalogação de dados e governança, onde esse dado fosse aprovado, iria ser enviado para a camada raw
+    - Raw: Dado bruto disponível para tratamento ou alguma validação de destes dados
+    - Processing: Dados tratados ja com dtypes definidos e processos de data quatily
+    - Curated: Dados processados e enriquecidos com alguma regra de negócio
+
+## OBJETIVO:
+- A principio eu ia criar esse pipeline usando azure databricks, porém tive problemas de criação do cluster , por conta da minha , account ser free.
+- Mudei a ideia para implementar todo o pipe usando Azure Functions, porém não consegui finalizar o processo, esta incompleto.
+
+- Estou enviando  código caso queiram ainda, analisar o codigo, onde criei as functions no arquivo ultils.py e chamei essas funções no arquivo main.py
+
+
+# Resultados obtidos:
+- Consegui logar na azure via python, coletar um arquivo xml que coloquei no bucket raw, fiz alguns tratamentos de dados para gerar um data frame e salvar na camada processing em formato parquet particionado por year, month e day, porém ao salvar apresentou um erro de "TypeError: quote_from_bytes() expected bytes " Ate o presente momento não consegui resolver esse erro.
+- Não consegui fazer os try except no codigo e documentar as funções, mas a ideia era fazer as funções com as exceções e tratamentos de possiveis erros.
+
+- Diretorio context e arquivo transform-info.json, é um arquivo para poder inserir dados que podem ser reaproveitados no codigo, assim facilita a replicação do codigo
+- Diretorio notebook, foi onde fiz os testes
+- Arquivo Workflow no diretorio .github, era para implementar a automatização do deploy da infra terraform, porém apresentou alguns erros de autenticação com a azure.
+
--- utils.py
+++ utils.py
@@ -8,6 +8,8 @@ from datetime import datetime, timedelta
 import json
 import pytz
 from azure.storage.blob import BlobServiceClient
+import io
+import pyarrow.parquet as pq
 
 columns_convert_int = ['quantidade','aliquota_icms']
 data_types_nfe = {
@@ -48,39 +50,71 @@ def read_context(json_path: str):
     return context
 
 def set_dtypes(df, dtypes):
+    """
+    - Função para setar os dtypes das colunas escolhidas na geração do data frame
+
+    """
     for column in dtypes:
         #print(column)
         df[column] = df[column].astype(dtypes[column])
     return df
 
 def convert_float_to_int(df, columns):
+    """
+    - Função criada para solucionar um erro que estava dando onde os valores estavam vindo com casas decimais depois do ponto e 
+    não estava convertendo para int, tive que converter tudo para float e depois usar essa função para converter para int
+
+    """
     for column in columns:
         df[column] = df[column].astype(np.int64)
     return df
 
 def data_load():
+    """
+    - Função para coletar a data atual
+    
+    """
     brasil_fuso_horario = pytz.timezone("America/Sao_Paulo")
     data_atual = datetime.now(brasil_fuso_horario).strftime("%Y-%m-%d %H:%M:%S")
     return data_atual
 
 def cria_data_fonte_e_carga(df,coluna):
+    """
+    - Função criada para gerar o campo de data fonte que é data do evento, e data-carga que é a data que esta sendo executado o pipe
+    e termos essas informações para auditorias ou ajustes de possíveis erros.
+
+    """
     df['data_fonte'] = pd.to_datetime(df[coluna], format='%Y-%m-%d %H:%M:%S',errors='ignore').dt.strftime('%Y-%m-%d %H:%M')
     df['data_carga']  = data_load()
     return df
 
 def cria_coluna_year_month_day(df,coluna: str):
+    """
+    - Função para criar as colunas que seriam usadas no particionamento do arquivo .parquet
+
+    """
+
     df["year"] = df[coluna].str.slice(0,4)
     df["month"] = df[coluna].str.slice(5,7)
     df["day"] = df[coluna].str.slice(8,10)
     return df
 
 def connect_storage_account():
-    context = read_context('context/transform-info.json')
+    """
+    - Função criada para conectar-se a storage account da azure
+
+    """
+    context = read_context('../context/transform-info.json')
     connect_str = context['transform-info'][0]['connect_string']
     blob_service_client = BlobServiceClient.from_connection_string(connect_str)
     return blob_service_client
 
 def get_container_xml_raw(container : str, blob : str):
+    """
+    - Função criada para ler o container raw
+
+    """
+
     blob_service_client = connect_storage_account()
     raw_container_client = blob_service_client.get_container_client(container)
 
@@ -93,6 +127,11 @@ def get_container_xml_raw(container : str, blob : str):
     return xml
 
 def create_data_frame_danfe(xml):
+    """
+    - Função principal onde , lê o arquivo danfe xml, faz coleta das colunas , transforma os dados , para serem salvos no container processing
+
+    """
+
     # define o namespace
     namespace = {'nfe': 'http://www.portalfiscal.inf.br/nfe'}
 
@@ -133,23 +172,44 @@ def create_data_frame_danfe(xml):
         
         # adiciona o dicionário ao DataFrame
         df = df.append(prod_dict, ignore_index=True)
-        df['codigo_uf_emitente'] = cUF
-        df['codigo_nf'] = cNF
-        df['numero_nf'] = numero_nf
-        df['data_emissao'] = data_emissao
-        df['natureza_operacao'] = natOp
-        df['cnpj_emitente'] = CNPJ_emit
-        df['nome_emitente'] = nome_emitente
-        df['logadouro_emitente'] = logadouro_emitente
-        df['numero_emitente'] = numero_emitente       
-        df['bairro_emitente'] = bairro_emitente       
-        df['fone_emitente'] = fone_emitente         
-        df['nome_cliente'] = nome_cliente
-        return df
+    df['codigo_uf_emitente'] = cUF
+    df['codigo_nf'] = cNF
+    df['numero_nf'] = numero_nf
+    df['data_emissao'] = data_emissao
+    df['natureza_operacao'] = natOp
+    df['cnpj_emitente'] = CNPJ_emit
+    df['nome_emitente'] = nome_emitente
+    df['logadouro_emitente'] = logadouro_emitente
+    df['numero_emitente'] = numero_emitente       
+    df['bairro_emitente'] = bairro_emitente       
+    df['fone_emitente'] = fone_emitente         
+    df['nome_cliente'] = nome_cliente
+    return df
     
 def save_container_processing_parquet(df,blob_service_client):
+    """
+    - Função para salvar dados no container processing, em formato parquet
+    
+    """
+
     # SAve parquet processing .get_blob_client('danfe.parquet')
     processing_container_name = blob_service_client.get_container_client("processing")
-    parquet_blob_client = blob_service_client.get_container_client(processing_container_name).get_blob_client('danfe.parquet')
-    parquet_blob_data = df.to_parquet(parquet_blob_client, partition_cols=['year', 'month', 'day'], index=False, engine='fastparquet')
-    parquet_blob_client.upload_blob(parquet_blob_data, overwrite=True)
+    parquet_blob_name = 'danfe.parquet'
+    parquet_blob_name_bytes = bytes(parquet_blob_name, 'utf-8')
+    parquet_blob_client = blob_service_client.get_container_client(processing_container_name).get_blob_client(parquet_blob_name_bytes)
+
+    with io.BytesIO() as output:
+        df.to_parquet(output, partition_cols=['year', 'month', 'day'], index=False, engine='fastparquet')
+        data = output.getvalue()
+    
+    # Envia os dados para o blob storage
+    with io.BytesIO(data) as input:
+        parquet_dataset = pq.ParquetDataset(input)
+        parquet_blob_client.upload_blob(parquet_dataset.read().to_pybytes(), overwrite=True)
+
+    # Envia os dados para o blob storage
+    #parquet_blob_client.upload_blob(data, overwrite=True)
+
+
+    #parquet_blob_data = df.to_parquet(parquet_blob_client, partition_cols=['year', 'month', 'day'], index=False, engine='fastparquet')
+    #parquet_blob_client.upload_blob(parquet_blob_data, overwrite=True)
